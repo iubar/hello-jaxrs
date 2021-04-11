240e871a3f27fb7b07d2c706bd57c2e61bba8586
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2021-04-11 19:51:48 +0200

    .

--- scripts/run-on-payara-micro.bat
+++ scripts/run-on-payara-micro.bat
@@ -1,9 +1,12 @@
 @echo off
 
-ECHO Running from %~dp0
-CD %~dp0..
+SET mypath=%~dp0
+SET mypath=%mypath:~0,-1%
+
+ECHO Running from %mypath%
+CD %mypath%\..
 CALL mvn clean install
-cd target
+CD target
 REM wget -O payara-micro-5.jar https://s3-eu-west-1.amazonaws.com/payara.fish/Payara+Downloads/5.2021.2/payara-micro-5.2021.2.jar
 java -jar %MY_APPS%\java_tools\payara\payara-micro-5.2021.2.jar --deploy hello-jaxrs.war
-cd %~dp0
+CD ..\scripts
