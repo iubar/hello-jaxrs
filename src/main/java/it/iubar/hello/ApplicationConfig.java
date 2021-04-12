2bb239dc2a8f5a7fa85f8a13d4b07ec1de9cf935
Andrea Borgogelli Avveduti <borgogelli@users.noreply.github.com>
2021-04-12 09:37:55 +0200

    Update ApplicationConfig.java

--- src/main/java/it/iubar/hello/ApplicationConfig.java
+++ src/main/java/it/iubar/hello/ApplicationConfig.java
@@ -2,20 +2,11 @@ package it.iubar.hello;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
- 
 
 /**
  * Configures the Jakarta REST application.
- * 
  */
 @ApplicationPath("/")
 public class ApplicationConfig extends Application {
- 
-//    @Override
-//    public Set<Class<?>> getClasses() {
-//        Set<Class<?>> set = new HashSet<>();
-//        set.add(Hello.class);
-//        return set;
-//    }
 
 }
