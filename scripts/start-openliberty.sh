4732d2bcfa02ce4bce011d5d33d4432fceeaa6e0
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2021-04-12 09:46:22 +0200

    .

--- scripts/start-openliberty.sh
+++ scripts/start-openliberty.sh
@@ -14,3 +14,4 @@ echo "Open the browser at http://192.168.0.155:9080"
 
 # Apps drop-in folder: /opt/openliberty21/wlp/usr/servers/myServer/dropins
 # Hot Deployment https://ralph.blog.imixs.com/2020/03/26/openliberty-and-hot-deployment/
+# Open Liberty Cheat Sheet: https://aguibert.github.io/openliberty-cheat-sheet/
---
+++ src/test/java/it/iubar/hello/Test.java
@@ -0,0 +1,48 @@
+package it.iubar.hello;
+
+import java.net.URI;
+
+import org.glassfish.jersey.client.ClientConfig;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Configuration;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+ 
+
+	public class Test {
+
+	    public static void main(String[] args) {
+	    	ClientConfig config= new ClientConfig();
+			Client client = ClientBuilder.newClient(config);
+
+	        WebTarget target = client.target(getBaseURI());
+
+	        String response = target.path("rest").
+	                            path("hello").
+	                            request().
+	                            accept(MediaType.TEXT_PLAIN).
+	                            get(Response.class)
+	                            .toString();
+
+	        String plainAnswer =
+	                target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+	        String xmlAnswer =
+	                target.path("rest").path("hello").request().accept(MediaType.TEXT_XML).get(String.class);
+	        String htmlAnswer=
+	                target.path("rest").path("hello").request().accept(MediaType.TEXT_HTML).get(String.class);
+
+	        System.out.println(response);
+	        System.out.println(plainAnswer);
+	        System.out.println(xmlAnswer);
+	        System.out.println(htmlAnswer);
+	    }
+
+	    private static URI getBaseURI() {
+	        return UriBuilder.fromUri("http://localhost:8080/hello-jaxrs/hello").build();
+	    }
+ 
+}
