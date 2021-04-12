823acecc595f34880b882c48f4f7891024627aca
Andrea Borgogelli Avveduti <borgogelli@users.noreply.github.com>
2021-04-12 09:16:35 +0200

    Update start-openliberty.sh

--- scripts/start-openliberty.sh
+++ scripts/start-openliberty.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 cd /opt/openliberty21/wlp
+
 sudo bin/server start myServer
 
-# http://192.168.0.155:9080/
-#
+sudo bin/server status myServer
+
+sudo bin/server version myServer
+
+sudo bin/server list
+
+echo "Open the browser at http://192.168.0.155:9080"
+
 # Apps drop-in folder: /opt/openliberty21/wlp/usr/servers/myServer/dropins
 # Hot Deployment https://ralph.blog.imixs.com/2020/03/26/openliberty-and-hot-deployment/
