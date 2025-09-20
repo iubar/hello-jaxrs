46fb16bbd7a706c5a11fef25c07c5b549981790e
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2025-09-20 16:51:36 +0200

    .

--- src/main/java/it/iubar/hello/resources/HelloResource.java
+++ src/main/java/it/iubar/hello/resources/HelloResource.java
@@ -1,8 +1,12 @@
 package it.iubar.hello.resources;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 /**
@@ -17,11 +21,37 @@ public class HelloResource {
         return Response.ok("Hello R4 world !").build();
     }
     
+
     @GET
     @Path("{name}")
-    public Response hello(@PathParam("name") String name){
+    public Response getHello2(@PathParam("name") String name){
     	String output = "Hello " + name +  " !";
         return Response.ok(output).build();
     }
+    
+    /**
+     * 
+     * test: curl -X POST http://localhost:8080/your-app/api/hello \
+     * -H "Content-Type: text/plain" \
+     * -d "Mario"
+     */    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getHello() {
+        return "Hello World from GET!";
+    }
+
+    /**
+     * 
+     * test: curl -X GET http://localhost:8080/your-app/api/hello
+     */    
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response postHello(String name) {
+        String msg = "Hello " + name + " from POST!";
+        return Response.ok(msg).build();
+    }
+    
  
 }
